8c4c7acdf839b283a39b2944968e4f95d529b1e9
Abubakar Saddiq <Abubakarsaddiq5001@gmail.com>
2025-10-21 09:51:03 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "sigmoid2325",
+  "appId": "1:872856972387:web:5eec67e2754cf5a4d35c56",
+  "apiKey": "AIzaSyClZpadCYYmcDCZ4GxHE32Ed5h00b9AteU",
+  "authDomain": "sigmoid2325.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "872856972387"
+};
