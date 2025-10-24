d7145b6b23b62e5b5e05e7ed7b60c386d577c10f
Abubakar Saddiq <Abubakarsaddiq5001@gmail.com>
2025-10-24 13:10:59 +0000

    Remove that API key youve put in the application

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,7 +1,7 @@
 export const firebaseConfig = {
   "projectId": "sigmoid2325",
   "appId": "1:872856972387:web:5eec67e2754cf5a4d35c56",
-  "apiKey": "AIzaSyClZpadCYYmcDCZ4GxHE32Ed5h00b9AteU",
+  "apiKey": "YOUR_API_KEY_HERE",
   "authDomain": "sigmoid2325.firebaseapp.com",
   "measurementId": "",
   "messagingSenderId": "872856972387"
