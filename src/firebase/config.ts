10a8009e157e1cb3e4a09cbc9ccbc8b1f42606d0
Abubakar Saddiq <Abubakarsaddiq5001@gmail.com>
2025-10-24 13:13:46 +0000

    AIzaSyClZpadCYYmcDCZ4GxHE32Ed5h00b9AteU

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,7 +1,7 @@
 export const firebaseConfig = {
   "projectId": "sigmoid2325",
   "appId": "1:872856972387:web:5eec67e2754cf5a4d35c56",
-  "apiKey": "YOUR_API_KEY_HERE",
+  "apiKey": "AIzaSyClZpadCYYmcDCZ4GxHE32Ed5h00b9AteU",
   "authDomain": "sigmoid2325.firebaseapp.com",
   "measurementId": "",
   "messagingSenderId": "872856972387"
