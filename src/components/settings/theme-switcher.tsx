c1b19ea79f50dd105840919a1d547cbc789a7baf
Abubakar Saddiq <Abubakarsaddiq5001@gmail.com>
2025-11-13 11:46:04 +0000

    Try fixing this error: `Runtime Error: Hydration failed because the serv

--- src/components/settings/theme-switcher.tsx
+++ src/components/settings/theme-switcher.tsx
@@ -1,11 +1,22 @@
+
 'use client';
 
 import { useTheme } from 'next-themes';
 import { Label } from '@/components/ui/label';
 import { RadioGroup, RadioGroupItem } from '@/components/ui/radio-group';
+import { useEffect, useState } from 'react';
 
 export function ThemeSwitcher() {
   const { setTheme, theme } = useTheme();
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) {
+    return null;
+  }
 
   return (
     <div className="space-y-4">
